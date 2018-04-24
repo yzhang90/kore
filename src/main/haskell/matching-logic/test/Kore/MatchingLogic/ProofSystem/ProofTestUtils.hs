cf6cd42e8130629762d80321268c196238f8e65d
Manasvi Saxena <msaxena2@illinois.edu>
2018-04-24 09:09:02 -0500

    Temp !! WIP

--- src/main/haskell/matching-logic/test/Kore/MatchingLogic/ProofSystem/ProofTestUtils.hs
+++ src/main/haskell/matching-logic/test/Kore/MatchingLogic/ProofSystem/ProofTestUtils.hs
@@ -24,7 +24,8 @@ import           Kore.MatchingLogic.ProofSystem.Minimal.Syntax
 import           Data.Kore.Parser.ParserImpl                  (sortParser,
                                                                unifiedVariableOrTermPatternParser,
                                                                symbolParser)
-
+import           Data.Kore.MetaML.MetaToKore
+import           Kore.MatchingLogic.ProofSystem.MLProofSystem (formulaVerifier)
 
 newtype NewGoalId = NewGoalId Int
 newtype GoalId = GoalId Int
@@ -59,25 +60,49 @@ type MLProofCommand =
 
 {-
  - Parsers for proof object
+ - In order to process object level proof objects, we need the  
+ - following parsers - 
+ -  1. CommandParser: Add/Derive commands need
+ -     - Parser Id -> Parser Formula -> Parser (rule id) 
+ -     - Use parseCommand :: Parser Id ->  Parser Formula -> Parser Rule
+ -
+ -  2. RuleParser: Parser different rules     
+ -     - Use parseMLRule :: Parser Sort -> Parser Label -> Parser Variable 
+ -                          -> Parser Formula
+ -  
  -}
-goalIdParser       :: Parser GoalId 
-objectSortParser   :: Parser (Sort Object)
-objectSymbolParser :: Parser (SymbolOrAlias Object)
-formulaParser      :: Parser UnifiedPattern
-commandParser      :: Parser (MLProofCommand)
-ruleParser         :: Parser (MLRule
-                               (Sort Meta)
-                               (SymbolOrAlias Meta)
-                               (Variable Meta)
-                               (MetaMLPattern Variable)
-                             )
+goalIdParser              :: Parser GoalId
+objectSortParser          :: Parser (Sort          Object)
+metaViaObjectSortParser   :: Parser (Sort          Meta)
+objectSymbolParser        :: Parser (SymbolOrAlias Object)
+metaViaObjectSymbolParser :: Parser (SymbolOrAlias Meta)
+formulaParser             :: Parser UnifiedPattern
+metaViaFormulaParser      :: Parser (MetaMLPattern Variable)
+testCommandParser         :: Parser MLProofCommand
+testRuleParser            :: Parser (MLRule
+                                      (Sort          Meta)
+                                      (SymbolOrAlias Meta)
+                                      (Variable      Meta)
+                                      (MetaMLPattern Variable)
+                                    )
+
+goalIdParser               = GoalId                             $    read <$> some digitChar
+objectSortParser           = sortParser Object
+metaViaObjectSortParser    = objectSortParser                   >>= return $ patternKoreToMeta
+objectSymbolParser         = symbolParser Object
+metaViaObjectSymbolParser  = objectSymbolParser                 >>= return $ patternKoreToMeta
+formulaParser              = unifiedVariableOrTermPatternParser
+metaViaObjectPatternParser = formulaParser                      >>= return $ patternKoreToMeta
+
+testRuleParser             = parseMLRule 
+                              metaViaObjectSortParser 
+                              metaViaObjectSymbolParser
+                              metaViaVariableParser 
+                              metaViaFormulaParser
+                              goalIdParser
+
+testCommandParser          = parseCommand goalIdParser metaViaFormulaParser ruleParser
 
 
-goalIdParser       = GoalId $ read <$> some digitChar
-objectSortParser   = sortParser Object
-objectSymbolParser = symbolParser Object
-ruleParser         = toMeta $ parseMLRule objectSortParser objectSymbolParser goalIdParser
-formulaParser      = unifiedVariableOrTermPatternParser
-commandParser      = parseCommand goalIdParser formulaParser ruleParser 
 
-
+testFormulaVerifier = formulaVerifier testIndexedModule
